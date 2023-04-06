f3b9586b3aa99dd9f7c7700bfc998ca841be855f
Soloc0der <106468074+Soloc0der@users.noreply.github.com>
2023-04-06 17:51:16 +0500

    v1.0.2

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h87a)=j&ddg-nd_ec6#v#h$*!zup!bowt@q3pw3eb$0(46uk6p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
